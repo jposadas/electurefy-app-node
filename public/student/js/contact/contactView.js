97748dbff5f044f6b084a6e7410ece7e4ab91650
Juan Posadas <jposadas@stanford.edu>
2014-12-02 13:49:36 -0800

    Ready to start working tonight

--- public/student/js/contact/contactView.js
+++ public/student/js/contact/contactView.js
@@ -1,8 +1,10 @@
 define(['hbs!js/contact/contact'], function(viewTemplate) {
     var $ = Framework7.$;
- 
+ 	console.log($);
+
     function render(params) {
 
+    	var boltTime = params.model
         $('.contact-page .bolt-list ul').html(viewTemplate(params.model));
  
     }
