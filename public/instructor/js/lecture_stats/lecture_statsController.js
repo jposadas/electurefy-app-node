5fdc59fb0d84592fa68b134e2546e72f7a31cb0c
Juan Posadas <jposadas@stanford.edu>
2014-12-05 06:09:44 -0800

    added sidebar to lecure stats

--- public/instructor/js/lecture_stats/lecture_statsController.js
+++ public/instructor/js/lecture_stats/lecture_statsController.js
@@ -4,12 +4,27 @@ define(["app","js/lecture_stats/lecture_statsView"], function(app, Lecture_stats
     var contact = null;
  
     function init(query){
-        Lecture_statsView.render({model: 
-        	{	lectureNum: query.lectureNum,
-        		lectureDate: query.lectureDate,
-                lectureDescription: query.lectureDescription
-        	}
+
+        var dbQuery = new Parse.Query("PreviousLectures");
+        dbQuery.equalTo('LectureNum', parseInt(query.lectureNum));
+        dbQuery.descending('BoltNum').find({
+            success: function(bolts) {
+                console.log('success');
+                console.log(bolts);
+                // Lecture_stats_past_boltsView.render({ model: bolts, lectureNum: lectureNum });
+                Lecture_statsView.render({ model: 
+                    {   lectureNum: query.lectureNum,
+                        lectureDate: query.lectureDate,
+                        lectureDescription: query.lectureDescription,
+                        pastBolts: bolts
+                    }
+                });
+            },
+            error: function(error, bolts) {
+                console.log('error: ' + error);
+            }
         });
+
     }
  
     return {
--- public/instructor/js/lecture_stats/lecture_statsView.js
+++ public/instructor/js/lecture_stats/lecture_statsView.js
@@ -2,7 +2,12 @@ define(['hbs!js/lecture_stats/lecture_stats'], function(viewTemplate) {
     var $ = Framework7.$;
  
     function render(params) {
-        $('.lecture_stats-page').html( viewTemplate() );
+
+        console.log(params.model.pastBolts);
+        $('.lecture_stats-page').html( viewTemplate({
+                pastBolts: params.model.pastBolts
+            })
+        );
 
         $('.lecture_stats-header').text("CS 247 Lecture Statistics");
         $('#lectureNumTitle').text("Lecture " + params.model.lectureNum);
